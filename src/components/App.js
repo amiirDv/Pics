cc6e92c4464e8f34cff507a901d2155b25588b86
amir yosef <amiryosef99@outlook.com>
2019-06-16 11:27:03 +0200

    v0.5

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 
-const App = () => {
-    return(
+class App extends React.Component  {
 
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
+
+render(){
+    return(
      <div className="ui container">
-         <SearchBar/> 
+         <SearchBar
+         onSubmit={this.onSearchSubmit}
+         /> 
     </div>
     );
 
 };
+}
 
 export default App;
